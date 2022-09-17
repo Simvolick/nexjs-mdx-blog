1256937df68f93cf9059f6ee7ab34c6a8fdf0426
Simvolick <simvolick@gmail.com>
2022-09-17 17:50:57 +0300

    More changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 const Nav = () => {
   return (
     <nav className='nav p-3 border-b-8'>
-    <Link href='/' passHref>
+    <Link href='/' >
         <h2 className='cursor-pointer'>Klim Yadrintsev</h2>
     </Link>
-    <Link href='/bio' passHref>
+    <Link href='/bio'>
         <h2 className='ms-5 cursor-pointer my-auto'>BIO</h2>
     </Link>
 
---
+++ src/pages/bio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Bio = () => {
+  return (
+    <div className='mt-3'>
+        <p className='mt-3 text-center'>I'm Klim Yadrintsev</p>
+        <p className='mt-3'>I'm a software engineer</p>
+    </div>
+  )
+}
+
+export default Bio
