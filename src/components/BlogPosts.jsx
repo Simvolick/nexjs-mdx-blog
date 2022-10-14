15def5f2844520b4eeb15db0c9e73c64dcc9b5ce
Simvolick <simvolick@gmail.com>
2022-10-14 12:56:06 +0200

    Hooray! Managed to make it work

--- src/components/BlogPosts.jsx
+++ src/components/BlogPosts.jsx
@@ -30,24 +30,4 @@ const BlogPosts = ( {posts} ) => {
   )
 }
 
-export const getStaticProps = async () => {
-  const files = fs.readdirSync(path.join('src', 'posts'))
-
-  const posts = files.map(filename => {
-    const markdownWithMeta = fs.readFileSync(path.join('src', 'posts', filename))
-    const { data:frontMatter } = matter(markdownWithMeta)
-     
-    return {
-      frontMatter,
-      slug: filename.split('.')[0]
-    }
-  })
-
-  return {
-    props: {
-      posts
-    }
-  }
-}
-
 export default BlogPosts
--- src/pages/trial.js
+++ src/pages/trial.js
@@ -1,13 +1,36 @@
+import fs from "fs"
+import * as path from "path"
+import matter from "gray-matter"
 import Link from "next/link"
 import Image from "next/image"
 import BlogPosts from "../components/BlogPosts"
 import BlogPosts2 from "../components/BlogPosts2"
 
 
-export default function Home() {
+export default function Home({posts}) {
   return (
     <div className="mt-5">
-        <BlogPosts2/>
+        <BlogPosts posts={posts}/>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const files = fs.readdirSync(path.join('src', 'posts'))
+
+  const posts = files.map(filename => {
+    const markdownWithMeta = fs.readFileSync(path.join('src', 'posts', filename))
+    const { data:frontMatter } = matter(markdownWithMeta)
+     
+    return {
+      frontMatter,
+      slug: filename.split('.')[0]
+    }
+  })
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
