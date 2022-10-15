0781b220eb53f3c5f51b90f4b56c8664638bb62b
Simvolick <simvolick@gmail.com>
2022-10-15 22:20:06 +0200

    managed to make the posts get sorted based on date

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -6,12 +6,11 @@ import Image from "next/image"
 import BlogPosts from "../../components/BlogPosts"
 
 
-export default function Home( { posts } ) {
+export default function Home( { posts, sortedPosts } ) {
   return (
     <div className="mt-5 flex flex-col items-center">
       <p className="-mt-5">All of the Blog Posts:</p>
-         <BlogPosts posts={posts}/>
-         {console.log(posts)}
+         <BlogPosts posts={sortedPosts}/>
     </div>
   )
 }
@@ -22,16 +21,25 @@ export const getStaticProps = async () => {
   const posts = files.map(filename => {
     const markdownWithMeta = fs.readFileSync(path.join('src', 'posts', filename))
     const { data:frontMatter } = matter(markdownWithMeta)
+    const dateIn = new Date(frontMatter.date)
+    const jsonDate = dateIn.toJSON()
      
     return {
       frontMatter,
-      slug: filename.split('.')[0]
+      slug: filename.split('.')[0],
+      jsonDate
     }
+  })
+
+  // I need to sort posts by jsonDate
+
+  const sortedPosts = posts.sort((a, b) => {
+    return new Date(b.jsonDate) - new Date(a.jsonDate)
   })
 
   return {
     props: {
-      posts
+      sortedPosts
     }
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,12 @@ import Link from "next/link"
 import Image from "next/image"
 import BlogPosts from "../components/BlogPosts"
 
-export default function Home( { posts } ) {
+
+
+export default function Home( { sortedPosts } ) {
   return (
     <div className="mt-5">
-        <BlogPosts posts={posts}/>
+        <BlogPosts posts={sortedPosts}/>
     </div>
   )
 }
@@ -19,16 +21,25 @@ export const getStaticProps = async () => {
   const posts = files.map(filename => {
     const markdownWithMeta = fs.readFileSync(path.join('src', 'posts', filename))
     const { data:frontMatter } = matter(markdownWithMeta)
+    const dateIn = new Date(frontMatter.date)
+    const jsonDate = dateIn.toJSON()
      
     return {
       frontMatter,
-      slug: filename.split('.')[0]
+      slug: filename.split('.')[0],
+      jsonDate
     }
+  })
+
+  // I need to sort posts by jsonDate
+
+  const sortedPosts = posts.sort((a, b) => {
+    return new Date(b.jsonDate) - new Date(a.jsonDate)
   })
 
   return {
     props: {
-      posts
+      sortedPosts
     }
   }
 }
