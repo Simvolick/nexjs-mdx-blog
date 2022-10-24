28c878c3b4c7618e5f3d95ea9393d2b96f82ac3e
Simvolick <simvolick@gmail.com>
2022-10-24 19:26:30 +0200

    Let's try this

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -25,39 +25,16 @@ const EmailForm = () => {
 const handleSubmit = async (event) => {
     // Stop the form from submitting and refreshing the page.
     event.preventDefault();
-
-    // Get data from the form.
-    const data = {
-      email: event.target.email.value,
-    };
-
-    // Send the data to the server in JSON format.
-    const JSONdata = JSON.stringify(data);
-
-    // API endpoint where we send form data.
-    const endpoint = "/api/airtable";
-
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
-      method: "POST",
-      // Tell the server we're sending JSON.
+    // Get the email from the form.
+    const email = event.target.email.value;
+    // Send the email to the API.
+    const response = await fetch('/api/airtable', {
+      method: 'POST',
+      body: JSON.stringify({ email }),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    };
-
-    // Send the form data to our forms API on Vercel and get a response.
-    const response = await fetch(endpoint, options);
-
-    // Get the response data from server as JSON.
-    // If server returns the name submitted, that means the form works.
-    const result = await response.json();
-    alert(`Thank you for signing up: ${result.data} 📨`);
-    return result;
-    
+    });
   };
 
 
