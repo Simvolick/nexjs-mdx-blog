0176016769ef662f01268b4c8a0a4daeb50c968e
Simvolick <simvolick@gmail.com>
2023-09-23 22:31:03 +0200

    Updated the styling issue

--- src/components/BlogPosts.jsx
+++ src/components/BlogPosts.jsx
@@ -1,33 +1,41 @@
-import fs from "fs"
-import * as path from "path"
-import matter from "gray-matter"
-import Link from "next/link"
-import Image from "next/image"
+import fs from "fs";
+import * as path from "path";
+import matter from "gray-matter";
+import Link from "next/link";
+import Image from "next/image";
 
-
-const BlogPosts = ( {posts} ) => {
+const BlogPosts = ({ posts }) => {
   return (
     <div className="mt-5 mx-5 text-center">
-        <div className="font-semibold text-5xl flex flex-auto justify-center ">
-          <div className="bg-orange-400 px-3 py-1 rounded-xl">All Posts:</div></div>
+      <div className="font-semibold text-5xl flex flex-auto justify-center ">
+        <div className="bg-orange-400 px-3 py-1 rounded-xl">All Posts:</div>
+      </div>
       {posts.map((post, index) => (
-          <Link href={'/blog/' + post.slug} passHref key={index}>
-          <div className="mt-3 text-center items-center cursor-pointer md:mx-32 lg:mx-52" >
+        <div className="mt-3 text-center items-center cursor-pointer md:mx-32 lg:mx-52" key={index}>
+            <Link href={"/blog/" + post.slug} passHref key={index}>
             <div className="m-2">
-            <h2 className="text-2xl font-medium">{post.frontMatter.title}</h2>
-            <p className="mt-3 text-center sm:mx-20 lg:mx-40">{post.frontMatter.description}</p>
-            <p className="mt-3 text-center text-sm text-gray-600">{post.frontMatter.date}</p>
-
+              <h2 className="text-2xl font-medium">{post.frontMatter.title}</h2>
+              <p className="mt-3 text-center sm:mx-20 lg:mx-40">
+                {post.frontMatter.description}
+              </p>
+              <p className="mt-3 text-center text-sm text-gray-600">
+                {post.frontMatter.date}
+              </p>
             </div>
             <div className="py-5 flex justify-center">
-              <Image className="rounded-3xl" src={post.frontMatter.thumbnailUrl} alt={post.frontMatter.title} width={500} height={350} />
+              <Image
+                className="rounded-3xl"
+                src={post.frontMatter.thumbnailUrl}
+                alt={post.frontMatter.title}
+                width={500}
+                height={350}
+              />
             </div>
-
+        </Link>
           </div>
-          </Link>
-        ))}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default BlogPosts
+export default BlogPosts;
